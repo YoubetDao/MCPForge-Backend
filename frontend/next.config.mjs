b5f1c10f5e5c560de0e4a947b78b7fbe2b22aa30
Dylan <dylanguo916@gmail.com>
2025-05-28 14:21:24 +0800

    refactor: remove Netlify redirects and API rewrites from configuration

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,20 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    // if the api url is not set, throw an error
-    if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
-      throw new Error('NEXT_PUBLIC_API_BASE_URL is not set');
-    }
-
-    const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${apiUrl}/api/:path*`,
-      },
-    ];
-  },
 }
 
 export default nextConfig
